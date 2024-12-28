71d4b869afecdef5ca1e0827cf6660c31ed2680d
J.V.G.I.E Prasad <ieprasad23@gmail.com>
2024-12-28 14:28:18 +0530

    Start to learn PHP from W3Schools

---
+++ W3Schools-PHP/01-PHP-Home.php
@@ -0,0 +1,17 @@
+PHP Tutorial 
+
+Learn PHP 
+
+  ** PHP is a sever scripting language, and a powerful tool for making dynamic 
+  and interactive Web pages.
+
+  ** PHP is a widely-used, free, and efficient alternative to competitors such as Microsoft's ASP.
+
+<!DOCTYPE html>
+<html>
+<body>
+  <?php
+    echo "My first PHP script";
+  ?>
+</body>
+</html>
