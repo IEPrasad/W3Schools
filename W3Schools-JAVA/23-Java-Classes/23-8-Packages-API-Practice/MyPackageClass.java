e808aa3aa311bf8d094018bf15335799f5d2236c
J.V.G.I.E Prasad <ieprasad23@gmail.com>
2024-12-30 03:29:36 +0530

    Packages/API in Java Programming

---
+++ W3Schools-JAVA/23-Java-Classes/23-8-Packages-API-Practice/MyPackageClass.java
@@ -0,0 +1,22 @@
+package mypack;
+class MyPackageClass {
+  public static void main(String[] args) {
+    System.out.println("This is my package!");
+  }
+}
+
+
+/*
+  compile >> C:\Users\Your Name>javac MyPackageClass.java
+  compile the package >> C:\Users\Your Name>javac -d . MyPackageClass.java
+
+
+  run MyPackageClass.java file 
+      >> C:\Users\Your Name>java mypack.MyPackageClass
+
+  The output will be:
+
+    >>> This is my package!
+
+*/
+
