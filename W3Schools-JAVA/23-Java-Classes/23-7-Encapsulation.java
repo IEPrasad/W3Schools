37219d833a0d4a329fd77c64eecb194073cfe75d
J.V.G.I.E Prasad <ieprasad23@gmail.com>
2024-12-29 18:00:16 +0530

    Updated

--- W3Schools-JAVA/23-Java-Classes/23-7-Encapsulation.java
+++ W3Schools-JAVA/23-Java-Classes/23-7-Encapsulation.java
@@ -42,7 +42,54 @@ Example explained
   ** However, as the name variable is declared as private, we cannot access it from outside this class:
 
   
+Example 
 
+  public class Main {
+    public static void main(String[] args) {
+      Person myObj = new Person();
+      myObj.name = "John";    // error
+      System.out.println(myObj.name)    // error
+    }
+  }
 
+** If the variable was declared as public, we would expect the following output:
+    >> > John 
 
+// ** However, as we try to access a private variable, we get an error:
+/*
+like this 
+
+  MyClass.java:4: error: name has private access in Person
+      myObj.name = "John";
+          ^
+  MyClass.java:5: error: name has private access in Person
+      System.out.println(myObj.name);
+                    ^
+  2 errors
+*/
+---
+
+** Instead, we use the getName() and setName() methods to access and update the variable:
+
+Example
+
+  public class Main() {
+    public static void main(String[] args) {
+      Person myObj = new Person();
+      myObj.setName("John");    // Set the value of the name variable to "John"
+      System.out.println(myObj.getName());
+    }
+  }
+  
+  >>> Outputs "John"
+
+---
+
+Why Encapsulation ?
+
+  ** Better control of class attributes and methods 
+  ** Class attributes can be made read-only (if you only use the get method),
+  or write-only (if you only use set method)
+  ** Flexible: the programmer can change one part of the code without affecting other parts 
+  ** Increased security of data
 
