9f4f25c11e799baf59f3f751112a8394477bfc21
J.V.G.I.E Prasad <ieprasad23@gmail.com>
2025-01-01 23:45:16 +0530

    PHP installation

---
+++ W3Schools-PHP/03-PHP-Install.php
@@ -0,0 +1,51 @@
+PHP Install 
+
+What Do I Need?
+  
+  ** To start using PHP, you can:
+
+    > Find a web host with PHP and MySQL support
+    > Install a web server on your own PC, and then install PHP and MySQL
+
+** Use a Web Host With PHP Support
+
+  ** If your server has activated support for PHP you do not need to do anything.
+
+  ** Just create some .php files, place them in your web directory, and the server will parse them for you.
+
+  ** You do not need to compile anything or install any extra tools.
+
+  ** Because PHP is free, most web hosts offer PHP support.
+
+---
+
+Set Up PHP on Your Own PC
+
+  ** However, if your server does not support PHP, you must:
+
+    > install a web server
+    > install PHP
+    > install a database, such as MySQL
+
+    ** The official PHP website (PHP.net) has installation instructions for PHP: http://php.net/manual/en/install.php
+
+---
+
+PHP Online Compiler / Editor 
+
+  ** With w3schools' online compiler, you can edit PHP code, and view the result in your browser:
+
+    > https://www.w3schools.com/php/phptryit.asp 
+
+---
+
+PHP Version 
+
+  ** To check your php version you can use the phpversion() function:
+
+Example 
+
+  ** Display the PHP version:
+  
+    echo phpversion();
+
