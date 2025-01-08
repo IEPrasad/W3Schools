8d3cb256a71bd870c4f0e2d0d630d7510816c311
J.V.G.I.E Prasad <ieprasad23@gmail.com>
2025-01-08 16:47:23 +0530

    Modified

--- W3Schools-JAVA/23-Java-Classes/23-17-ArrayList.java
+++ W3Schools-JAVA/23-Java-Classes/23-17-ArrayList.java
@@ -23,3 +23,106 @@ Example
 
 ---
 
+Add Items
+
+  ** The ArrayList class has many useful methods. For example, to add elements to the list, use the add() method:
+
+Example
+
+  import java.util.ArrayList;
+
+  public class Main {
+    public static void main(String[] args) {
+      ArrayList<String> cars = new ArrayList<String>();
+      cars.add("Volvo");
+      cars.add("BMW");
+      cars.add("Ford");
+      cars.add("Mazda");
+      System.out.println(cars);
+    }
+  }
+
+  >>>
+    [Volvo, BMW, Ford, Mazda]
+  
+---
+
+** You can also add an item at a specified position by referring to the index number:
+
+Example
+
+  import java.util.ArrayList;
+
+  public class Main {
+    public static void main(String[] args) {
+      ArrayList<String> cars = new ArrayList<String>();
+      cars.add("Volvo");
+      cars.add("BMW");
+      cars.add("Ford");
+
+      cars.add(0, "Mazda");   // Insert element at the beginning of the list (0)
+
+      System.out.println(cars);
+    }
+  }
+
+  >>>
+    [Mazda, Volvo, BMW, Ford]
+
+---
+
+** Remember: Array indexes start with 0: [0] is the first element. [1] is the 
+second element, etc.
+
+---
+
+Access an Item
+
+  ** To access an element in the ArrayList, use the get() method and refer to the index number:
+
+Example
+
+  cars.get(0);
+
+-->>
+
+  import java.util.ArrayList;
+
+  public class Main {
+    public static void main(String[] args) {
+      ArrayList<String> cars = new ArrayList<String>();
+      cars.add("Volvo");
+      cars.add("BMW");
+      cars.add("Ford");
+      cars.add("Mazda");
+      System.out.println(cars.get(0));
+    }
+  }
+
+---
+ 
+Change an Item 
+
+  ** To modify an element, use the set() method and refer to the index number:
+
+Example
+
+  cars.remove(0)
+
+---
+  import java.util.ArrayList;
+
+  public class Main {
+    public static void main(String[] args) {
+      ArrayList<String> cars = new ArrayList<String>();
+      cars.add("Volvo");
+      cars.add("BMW");
+      cars.add("Ford");
+      cars.add("Mazda");
+      cars.remove(0);
+      
+      System.out.println(cars);
+    }
+  }
+
+
