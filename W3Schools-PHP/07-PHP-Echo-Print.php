a30702306cc4786086757b532c33f35a06029897
J.V.G.I.E Prasad <ieprasad23@gmail.com>
2025-01-11 11:27:52 +0530

    Echo and Print in PHP

--- W3Schools-PHP/07-PHP-Echo-Print.php
+++ W3Schools-PHP/07-PHP-Echo-Print.php
@@ -96,3 +96,57 @@ Example
     print("Hello");
   ?>
 
+---
+
+Display Text 
+
+  ** The following example shows how to output text with the print command 
+  (notice that the text can contain HTML markup):
+
+Example
+
+  <?php
+    print "<h2> PHP is Fun!</h2>";    
+    print "Hello world!<br>";    
+    print "I'm about to learn PHP!";    
+  ?>
+
+---
+
+Display Variables 
+
+  ** The following example shows how to output text and variables with the print 
+  statement:
+
+Example 
+
+  <?php 
+    $txt1 = "Learn PHP";
+    $txt2 = "W3Schools.com";
+    
+    print "<h2>$txt1</h2>";
+    print "<p>Study PHP at $txt2</p>";
+  ?>
+
+---
+
+Using Single Quotes 
+  
+  ** Strings are surrounded by quotes, but there is a difference between single and double quotes in PHP.
+
+  ** When using double quotes, variables can be inserted to the string as in the example above.
+
+  ** When using single quotes, variables have to be inserted using the . operator, like this:
+
+Example
+
+  <?php 
+    $txt1 = "Learn PHP";
+    $txt2 = "W3Schools.com";
+
+    print '<h2>' . $txt1 . '</h2>';
+    print '<p>Study PHP at ' . $txt2 . '</p>';
+  ?>
+
+---
+
