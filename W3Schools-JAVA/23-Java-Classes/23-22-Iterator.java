26de1b4795f8abfd2b2cf63b2295bfd2ca33ae6c
J.V.G.I.E Prasad <ieprasad23@gmail.com>
2025-01-21 23:34:05 +0530

    Updated

--- W3Schools-JAVA/23-Java-Classes/23-22-Iterator.java
+++ W3Schools-JAVA/23-Java-Classes/23-22-Iterator.java
@@ -38,7 +38,54 @@ Example
 
 ---
 
-Looping Through a Collction 
+Looping Through a Collection 
 
   ** To loop through a collection, use the hashNext() and next()
   method of the Iterator:
+
+Example 
+
+  while (it.hasNext()) {
+    System.out.println(it.next());
+  }
+
+---
+
+Removing Items from a Collection
+
+  ** Iterators are designed to easily change the collection that 
+  they loop. The remove() method can remove items from collection while looping.
+
+Example
+
+  ** Use an iterator to remove numbers less than 10 from a collection:
+
+  import java.util.ArrayList;
+  import java.util.Iterator;
+
+  public class Main {
+    public static void main(String[] args) {
+      ArrayList<Integer> numbers = new ArrayList<Integer>();
+      numbers.add(12);
+      numbers.add(8);
+      numbers.add(2);
+      numbers.add(23);
+
+      Iterator<Integer> it = numbers.iterator();
+      while (it.hasNext()) {
+        Integer i = it.next();
+        if(i < 10) {
+          it.remove();
+        }
+      }
+      System.out.println(numbers);
+    } 
+  }
+
+---
+
+** Note: Trying to remove items using a for loop or a for-each loop would not
+work correctly because the collection is changing size at the same time 
+that the code is trying to loop.
+
+                        ----
