e4485a190189234de9ef78be21349a5c4fa087ff
J.V.G.I.E Prasad <ieprasad23@gmail.com>
2025-02-11 23:59:19 +0530

    Updated

--- W3Schools-JAVA/23-25-Threads.java
+++ W3Schools-JAVA/23-25-Threads.java
@@ -28,3 +28,42 @@ Creating a Thread
       }
     }
 
+---
+
+Running Threads 
+
+  ** If the class extends the Thread class, the thread can be run by creating an
+  instance of the class and call its start() method: 
+
+Extend Example 
+
+  public class Main extends Thread {
+    public static void main(String[] args) {
+      Main thread = new Main();
+      thread.start();
+      System.out.println("This code is outside of the thread");
+    }
+
+    public void run() {
+      System.out.println("This code is running in a thread");
+    }
+  }
+
+  ** If the class implements the Runnable interface, the thread can be run by 
+  passing an instance of the class to a Thread object's constructor and then calling the thread's start() method:
+
+Implement Example
+
+  public class Main implements Runnable {
+    public static void main(String[] args) {
+      Main obj = new Main();
+      Thread thread = new Thread(obj);
+      thread.start();
+      System.out.println("This code is outside of the thread");
+    }
+
+    public void run() {
+      System.out.println("This code is running in a thread");
+    }
+  }
+
