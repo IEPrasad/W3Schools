a1c179326990929818dcc99ca0d863f1733a07e5
J.V.G.I.E Prasad <ieprasad23@gmail.com>
2025-02-17 23:37:32 +0530

    Updated

--- W3Schools-JAVA/25-Java-Advanced/25-1-Exceptions.java
+++ W3Schools-JAVA/25-Java-Advanced/25-1-Exceptions.java
@@ -104,5 +104,25 @@ The throw keyword
 
   Example 
 
-  ** Throw an exception if age is below 18 (print "Access denied"). If age is 18 or older, print "Access granted".
+  ** Throw an exception if age is below 18 (print "Access denied"). If age is 18 or older, print "Access granted":
 
+  public class Main {
+    static void checkAge(int age) {
+      if (age < 18) {
+        throw new ArithmeticException("Access denied - You must be at least 18 years old.");
+      }
+      else {
+        System.out.println("Access granted - You are old enough!");
+      }
+    }
+
+    public static void main(String[] args) {
+      checkAge(15); // Set age to 15 (which is below 18...)
+    }
+  }
+
+  The output will be:
+
+  Exception in thread "main" java.lang.ArithmeticException: Access denied - You must be at least 18 years old.
+          at Main.checkAge(Main.java:4)
+          at Main.main(Main.java:12)
