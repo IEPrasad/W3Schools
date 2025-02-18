00dcd10768f3719b09c19f0a8196d1ef665b9097
J.V.G.I.E Prasad <ieprasad23@gmail.com>
2025-02-18 23:48:45 +0530

    Files in Java

---
+++ W3Schools-JAVA/26-Java-File-Handling/26-1-Files.java
@@ -0,0 +1,19 @@
+Java Files 
+
+  ** File handling is an important part of any application. 
+
+  ** Java has several methods for creating, reading, updating, and deleting files. 
+
+Java File Handling 
+
+  ** The file class from the java.io package, allows us to work with files. 
+
+  ** To use the File class, create and object of the class, and specify the filename or directory name:
+
+Example 
+
+  import java.io.File;    // Import the File class 
+  
+  File myObj = new File("Filename.txt");    // Specify the filename 
+
+  
