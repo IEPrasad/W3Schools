e8292eed452aead831b6826d031b65b1123a1b9d
J.V.G.I.E Prasad <ieprasad23@gmail.com>
2025-02-22 23:39:15 +0530

    Introduction of React

---
+++ W3Schools-React/01-React-Tutorial/1-2-Intro.js
@@ -0,0 +1,24 @@
+React Introduction 
+
+What is React?
+
+  ** React, sometimes referred to as a frontend JavaScript framework, is a JavaScript library created by Facebook. 
+
+  ** React is a tool for building UI components. 
+
+How does React Work?
+
+  ** React creates a VIRTUAL DOM in memory.
+
+    ** Instead of manipulating the browser's DOM directly, React creates a 
+    virtual DOM in memory, where it does all the necessary manipulating, before 
+    making the changes in the browser DOM.
+
+  ** React only changes what needs to be changed!
+
+    ** React finds out what changes have been matchMedia, and changes only what 
+    needs to be changed. 
+
+    ** You will learn the various aspects of how React does in the rest of this tutorial. 
+
+    
