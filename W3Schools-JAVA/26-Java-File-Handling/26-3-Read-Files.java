51cc180554129bd7838f104bb40cb9316e487b14
J.V.G.I.E Prasad <ieprasad23@gmail.com>
2025-02-23 14:31:39 +0530

    Updated

--- W3Schools-JAVA/26-Java-File-Handling/26-3-Read-Files.java
+++ W3Schools-JAVA/26-Java-File-Handling/26-3-Read-Files.java
@@ -34,3 +34,47 @@ Example
     Files in Java might be tricky, but it is fun enough!
 
 ---
+
+
+Get File Information 
+
+  ** To get mroe information about a file, use any of the File methods:
+
+Example
+
+  import java.io.File;    // Import the File class 
+
+  public class GetFileInfo {
+    public static void main(String[] args) {
+      File myObj = new File("filename.txt");
+      if (myObj.exists()) {
+        System.out.println("File name: " + myObj.getName());
+        System.out.println("Absolute path: " + myObj.getAbsolutePath());
+        System.out.println("Writeable: " + myObj.canWrite());
+        System.out.println("Readable: " + myObj.canRead());
+        System.out.println("File size in bytes: " + myObj.length());
+      } else {
+        System.out.println("The file does not exists.");
+      }
+    }
+  }
+
+  The output will be:
+
+  >>>
+    FIle name: filename.txt 
+    Absolute path: C:\Users\MyName\filename.txt
+    Writeable: true 
+    Readable: true
+    File size in bytes: 0
+
+---
+
+** Note: There are many available classes in the Java API that can be used to read and write files in Java: FileReader, BufferedReader, Files, Scanner, FileInputStream, FileWriter, BufferWriter, FileOutputStream, etx. Which one to use
+depends on the Java version you're working with and whether you need to read bytes or characters, and the size of the file/lines etc.
+
+Tip: To delete a file, read our Java Delete Files Chapter. 
+    >> https://www.w3schools.com/java/java_files_delete.asp
+'
+
+                    ----
