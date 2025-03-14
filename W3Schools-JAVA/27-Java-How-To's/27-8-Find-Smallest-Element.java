70d45679b68f675514aa94ca9fe2cd36e9813d8e
J.V.G.I.E Prasad <ieprasad23@gmail.com>
2025-03-14 23:46:22 +0530

    Updated

---
+++ W3Schools-JAVA/27-Java-How-To's/27-8-Find-Smallest-Element.java
@@ -0,0 +1,33 @@
+Learn Java 
+
+  ** Java is a popular programming language. 
+  
+  ** Java is used to develop mobile apps, web apps, desktop apps, games and much more.
+
+Example in Each Chapter 
+
+  ** Our "Try it Yourself" editor makes it easy to learn Java. You can edit Java code and view the result in your browser.
+
+Example
+
+  public class Main {
+    public static void main(String[] args) {
+      System.out.println("Hello World");
+    }
+  }
+
+---
+
+** Click on the "Run example" button to see how it works. 
+
+** We recommend reading this tutorial, in the sequence listed in the left menu.
+
+** Java is an object oriented language and some may be new. Take breaks when needed, and go over the examples as many times as needed. 
+
+---
+
+Java Exercises
+  
+  ** Many chapters in this tutorial end with an exercise where you can check your level of knowledge. 
+
+              ----
