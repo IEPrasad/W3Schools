50b26f8c32079e6a17f3303b2c9258b8bced6d34
J.V.G.I.E Prasad <ieprasad23@gmail.com>
2025-03-16 23:59:35 +0530

    Positive or Negative in Java

---
+++ W3Schools-JAVA/27-Java-How-To's/27-14-Positive-or-Negative.java
@@ -0,0 +1,13 @@
+Java How To Find Positive or Negative Numbers 
+
+Find Out if a Number is Positive or Negative 
+
+  ** Find out if a number is even or odd:
+
+Example 
+
+  int myNum = 10; // Is this a positive or negative number?
+
+  if (myNum > 0) {
+    System.out.println("The value is a positive number.");
+  } else if (myNum < 0)
