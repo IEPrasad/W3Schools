7d03b3bfe34b6f3244199cbd8fae1bf8c43cdd8c
J.V.G.I.E Prasad <ieprasad23@gmail.com>
2025-03-16 23:56:21 +0530

    Even or Odd numbers in Java

---
+++ W3Schools-JAVA/27-Java-How-To's/27-13-Even-or-Odd-Number.java
@@ -0,0 +1,31 @@
+Java How To Find Even or Odd Numbers 
+
+Check Whether a Number is Even or Odd 
+
+  ** Find out if a number is even or odd:
+
+Example 
+
+  int number = 5;
+
+  // Find out if the number is even or odd
+  if (number % 2 == 0) {
+    System.out.println(number + " is even.");
+  } else {
+    System.out.println(number + " is odd.");
+  }
+
+---
+
+Related Pages 
+
+  ** Java Numbers Tutorial 
+      >> https://www.w3schools.com/java/java_data_types_numbers.asp
+  
+  ** Java Operators Tutorial 
+      >> https://www.w3schools.com/java/java_operators.asp
+
+  ** Java Math Tutorial 
+      >> https://www.w3schools.com/java/java_math.asp
+
+        ----
