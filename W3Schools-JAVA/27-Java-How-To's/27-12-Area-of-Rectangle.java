4715ec1038863a4d9e3c9d7ffb36944e33654cde
J.V.G.I.E Prasad <ieprasad23@gmail.com>
2025-03-16 23:49:52 +0530

    How to get a area of a Rectangle in Java

---
+++ W3Schools-JAVA/27-Java-How-To's/27-12-Area-of-Rectangle.java
@@ -0,0 +1,15 @@
+Java How To Get the Area of a Rectangle 
+
+Area of Rectangle 
+
+  ** The area of a rectangle can be found by multiplying  the length and of the rectangle by the width:
+
+Example
+
+  int length = 5;
+  int width = 2;
+  int area = length * width;
+  System.out.println("The area of the rectangle is: " + area);
+
+---
+        ----
