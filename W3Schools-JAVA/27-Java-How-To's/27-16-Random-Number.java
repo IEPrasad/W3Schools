d2822cf38dfdd4db2d77fe42b21adea2b90b6602
J.V.G.I.E Prasad <ieprasad23@gmail.com>
2025-03-19 23:47:05 +0530

    Random Number in Java

---
+++ W3Schools-JAVA/27-Java-How-To's/27-16-Random-Number.java
@@ -0,0 +1,33 @@
+Java How To Generate Random Numbers 
+
+How To Generate a Random Number 
+
+  ** You can use Math.random() method to generate a random number. 
+
+  ** Math.random() returns a random number between 0.0 (inclusive), and 1.0 (exclusive):
+
+Example
+
+  Math.random();
+
+** To get more control over the random number, for example, if you only want a random number between 0 and 100, you can use the following formula:
+
+Example
+
+  int randomNum = (int)(Math.random() * 101); 0 to 100
+
+---
+
+Related Pages 
+
+  Java Math Tutorial 
+    ** >> https://www.w3schools.com/java/java_math.asp
+  
+  Java Math Reference 
+    ** >> https://www.w3schools.com/java/java_ref_math.asp
+
+  Java Type Casting 
+    ** >> https://www.w3schools.com/java/java_type_casting.asp
+
+
+            ----
