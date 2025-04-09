3d16e1bfd941316d20ec9fbbc5d8e08a7995a9bd
J.V.G.I.E Prasad <126505521+IEPrasad@users.noreply.github.com>
2025-04-09 23:33:54 +0530

    Create 01-TS-Home.ts

---
+++ W3Schools-TypeScript/01-TS-Home.ts
@@ -0,0 +1,35 @@
+// TypeScript Tutorial 
+
+// Learn TypeScript 
+
+//   ** TypeScript is JavaScript with added syntax for types.
+
+// Examples in Each Chapter 
+
+//   ** One "Try it Yourself" editor makes it easy to learn TypeScript.
+
+//   ** You can edit TypeScript code and view the result in your browser. 
+
+// Example 
+
+//   console.log("Hello World!"); 
+
+// ---
+
+// Click on the "Try it Yourself" button to see how it works.
+
+  // ** We recommnend reading this tutorial in the sequence listed in the left menu.
+
+// ---
+
+// TypeScript Exercises
+
+//   ** Test Yourself with Exercises.
+
+// Exercises:
+
+  // ** Complete the sentence:
+
+// ** TypeScript allows developers to add types to JavaScript.
+
+// Start the Exercises
