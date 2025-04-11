6a9aed91cce49eddcdce28e68b27f0a9a9b6db96
J.V.G.I.E Prasad <ieprasad23@gmail.com>
2025-04-11 23:29:40 +0530

    TypeScript Get started

---
+++ W3Schools-TypeScript/03-TS-Get-Started.ts
@@ -0,0 +1,37 @@
+TypeScript Getting Started 
+
+TypeScript Compiler 
+
+  ** TypeScript is transpiled into JavaScript using a compiler.
+
+** TypeScript being converted into JavaScript means it runs anywhere that JavaScript runs! 
+
+
+Installing the Compiler 
+
+  ** TypeScript has an Official compiler which can be installed through npm.
+
+** Learn more about npm, and how to get stared here: What is npm? 
+    >> https://www.w3schools.com/whatis/whatis_npm.asp
+
+---
+
+** Withing your npm project, run the following command to install the compiler:
+
+  cmd: npm install typescript --save--dev 
+
+** Which should give you an output similer to:
+
+  cmd:>>>:  added 1 package, and audited 2 packages in 2s
+            found 0 vulnerabilities 
+
+** The compiler is installed in the node_modules directory and can be run with: npx tsc.
+
+  cmd: npx tsc 
+
+** Which should give you an output similar to:
+
+  cmd:>>>:  Version 4.5.5 
+            tsc: The TypeScript Compiler - Version 4.5.5 
+
+** Followed by a list of all Common Commands.
