2f246d5d17ff33dca2b7b53b5bab7967e4b9c66a
J.V.G.I.E Prasad <ieprasad23@gmail.com>
2025-04-11 23:59:04 +0530

    Updated

--- W3Schools-TypeScript/03-TS-Get-Started.ts
+++ W3Schools-TypeScript/03-TS-Get-Started.ts
@@ -35,3 +35,52 @@ Installing the Compiler
             tsc: The TypeScript Compiler - Version 4.5.5 
 
 ** Followed by a list of all Common Commands.
+
+-----
+-----
+
+Configuring the compiler
+
+  ** By default the TypeScript compiler will print a help message when run in an empty project.
+
+  ** The compiler can be configured using a tsconfig.json file.
+
+  ** You can have TypeScript create tsconfig.json with the recommended settings with:
+
+    cmd: npx tsc --init 
+
+  ** Which should give you an output similer to: 
+
+    cmd:>>>: 
+        Created a new tsconfig.json with:
+        TS
+          target: es2016
+          module: commonjs
+          strict: true
+          esModuleInterop: true
+          skipLibCheck: true
+          forceConsistentCasingInFileNames: true
+        
+        You can learn more at https://aka.ms/tsconfig.json
+
+  ** Here is an example of more things you could add to the tsconfig.json file: 
+
+          {
+            "include": ["src"],
+            "compilerOptions": {
+              "outDir": "./build"
+            }
+          }
+
+  ** You can open the file in an editor to add options. This will configure the TypeScrit compiler to 
+  transpile TypeScript files located in teh src/ direcotry of your project, into JavaScript files in 
+  the build/ directory.
+  
+
+** This is one way to quickly get started with TypeScript. There are many other options availabel such as a create-react-app template, a node starter project, and a webpack plugin.
+
+    >>  https://create-react-app.dev/docs/adding-typescript/
+    >> https://github.com/microsoft/TypeScript-Node-Starter
+    >> https://webpack.js.org/guides/typescript/
+    
+---
