4882e4d1a81dfd71489381cbad3d87406968dd13
J.V.G.I.E Prasad <ieprasad23@gmail.com>
2025-04-12 23:18:01 +0530

    Updated

--- W3Schools-React/01-React-Tutorial/1-2-Intro.js
+++ W3Schools-React/01-React-Tutorial/1-2-Intro.js
@@ -21,4 +21,24 @@ How does React Work?
 
     ** You will learn the various aspects of how React does in the rest of this tutorial. 
 
-    
+---
+
+What You Should Already Know
+ 
+  ** Before you continue you should have a basic understanding of the following:
+
+    >  HTML
+    >  CSS
+    >  JavaScript
+If you want to study these subjects first, find the tutorials on our Home page.
+
+React.JS History
+  Latest version of React.JS is 19.0.0 (December 2024).
+
+  Initial release to the Public (version 0.3.0) was in July 2013.
+
+  React.JS was first used in 2011 for Facebook's Newsfeed feature.
+
+  Facebook Software Engineer, Jordan Walke, created it.
+
+        ----
